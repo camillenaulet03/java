56d3db3128458363e7f285fc54b2c893f91dd665
Buffaløø <45590407+Bufaloo@users.noreply.github.com>
2019-02-20 11:45:13 +0100

    Add files via upload

---
+++ src/main/java/fr/imie/bank/model/BankAccount.java
@@ -0,0 +1,20 @@
+package fr.imie.bank.model;
+
+public class BankAccount {
+	public Integer number;
+	
+	
+	public BankAccount(Integer number) {
+		super();
+		this.number = number;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+	
+}
